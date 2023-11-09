8526b6113dc50bcd125ec15dd7ca2678cf3e0398
atmathias <atmathias@gmail.com>
2023-11-09 12:22:46 +0300

    start_commit

---
+++ further_ipe_analysis.R
@@ -0,0 +1,9 @@
+# Load libraries
+library(tidyverse)
+library(glue)
+library(supporteR)
+
+# load data
+supporteR::create_project_sub_folders()
+supporteR::add_gitignore_entries()
+supporteR::add_dot_gitkeep_to_folder()
